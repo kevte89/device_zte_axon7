d7c364ef2b5d62db488609c069ca034b3d80b7eb
kevte89 <39636900+kevte89@users.noreply.github.com>
2018-10-01 20:25:14 +0200

    Update and rename aokp.mk to aosp.mk

--- aosp.mk
+++ aosp.mk
@@ -22,12 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ailsa_ii device
 $(call inherit-product, device/zte/axon7/device.mk)
 
-# Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aokp_axon7
+PRODUCT_NAME := aosp_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
-PRODUCT_MODEL := ZTE A2017U
+PRODUCT_MODEL := ZTE A2017G
 PRODUCT_MANUFACTURER := ZTE
